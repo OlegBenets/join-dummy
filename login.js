2e76066b4dcd6202e96ea1a6837824568db507bc
Sascha Siegert <s.siegert85@googlemail.com>
2024-05-02 22:51:53 +0200

    login visual reddy

--- login.js
+++ login.js
@@ -1,9 +1,11 @@
 
 function loadPage() {
-    setTimeout(slideLogo, 175);
+    setTimeout(slideLogo, 250);
 }
 
 function slideLogo() {
-        let logo = document.getElementById('logo');
+    let logo = document.getElementById('logo');
     logo.classList.add('logo_slide');
+    let content = document.getElementById('content');
+    content.classList.remove('hidden');
 }
