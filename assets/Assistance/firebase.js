1b9f1a721a5072788d7de1d46492d1b43cdf5789
Oleg Benets <oleg.benets@web.de>
2024-05-08 20:08:17 +0200

    added favicon

---
+++ assets/Assistance/firebase.js
@@ -0,0 +1,57 @@
+function onloadFunc() {
+    console.log('test');
+    deleteData("");
+}
+
+const BASE_URL = 'https://remotestorage-dc45f-default-rtdb.europe-west1.firebasedatabase.app/';
+
+async function loadData(path="") {
+    let response = await fetch(BASE_URL + path + ".json"); // fetch default wert ist GET
+    return responseAsJson = response.json();
+}
+
+//CRUD-Operationen "Create, Read, Update, Delete"
+
+//POST
+async function postData(path="", data={}) {
+    let response = await fetch(BASE_URL + path + ".json",{
+        method: "POST",
+        header: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data)
+    });
+    return responseAsJson = response.json();
+}
+
+//DELETE
+async function deleteData(path="") {
+    let response = await fetch(BASE_URL + path + ".json",{
+        method: "DELETE",
+    });
+    return responseAsJson = response.json();
+}
+
+//PATCH
+async function patchData(path="", data={}) {
+    let response = await fetch(BASE_URL + path + ".json",{
+        method: "PATCH",
+        header: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data)
+    });
+    return responseAsJson = response.json();
+}
+
+//PUT
+async function putData(path="", data={}) {
+    let response = await fetch(BASE_URL + path + ".json",{
+        method: "PUT",
+        header: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data)
+    });
+    return responseAsJson = response.json();
+}
--- js/contact.js
+++ js/contact.js
@@ -1,5 +1,116 @@
+let contacts = [
+    {
+        'name': 'Anton Müller',
+        'email': 'anton@gmail.com'
+      },
+      {
+        'name': 'Anja Schulz',
+        'email': 'schulz@hotmail.com'
+      },
+      {
+        'name': 'Benedikt Ziegler',
+        'email': 'benedikt@gmail.com'
+      },
+      {
+        'name': 'David Eisenberg',
+        'email': 'davidberg@gmail.com'
+      },
+      {
+        'name': 'Eva Fischer',
+        'email': 'fischer@hotmail.com'
+      },
+      {
+        'name': 'Lisa Schmidt',
+        'email': 'schmidt@gmail.com'
+      },
+      {
+        'name': 'Maximilian Richter',
+        'email': 'maxi@gmail.com'
+      },
+      {
+        'name': 'Sarah Wagner',
+        'email': 'wagner@hotmail.com'
+      },
+      {
+        'name': 'Julia Mayer',
+        'email': 'julia.mayer@gmail.com'
+      },
+      {
+        'name': 'Michael Berger',
+        'email': 'michaelb@gmail.com'
+      },
+      {
+        'name': 'Laura Wolf',
+        'email': 'laura.wolf@hotmail.com'
+      },
+      {
+        'name': 'Alexander Klein',
+        'email': 'alex.klein@gmail.com'
+      },
+      {
+        'name': 'Sophie Hofmann',
+        'email': 'sophie.hofmann@hotmail.com'
+      },
+      {
+        'name': 'Simon Becker',
+        'email': 'simon.becker@gmail.com'
+      },
+      {
+        'name': 'Carolin Wagner',
+        'email': 'carolin.wagner@hotmail.com'
+      },
+      {
+        'name': 'Johannes Schneider',
+        'email': 'johannes.schneider@gmail.com'
+      },
+      {
+        'name': 'Laura Weber',
+        'email': 'laura.weber@hotmail.com'
+      }
+];
+
+function init() {
+    loadContacts();
+}
+
+function loadContacts() {
+    let allContacts = document.getElementById('all-contacts');
+    allContacts.innerHTML = '';
+    for (let i = 0; i < contacts.length; i++) {
+        const contact = contacts[i];
+        
+        allContacts.innerHTML += renderContacts(contact, i);
+    }
+}
+
+function renderContacts(contact, i) {
+    let names = contact.name.split(" ");
+    
+    let name = [];
+    for (let j = 0; j < names.length; j++) {
+        const element = names[j].charAt(0);
+        name.push(element);
+    } 
+    let fullName = name.join("");
+return /*html*/ `
+         <div class="sort-contacts"><h4>${contact.name.charAt(0).toUpperCase()}</h4></div>
+         <div class="border-container">
+         <div class="border-grey"></div>
+        </div>
+        <div onclick="showContact(${i})" class="contact" id="contact${i}">
+            <div class="image-container">
+                <span>${fullName}</span> 
+            </div>
+            <div class="contact-data">
+                <h4>${contact.name}</h4>
+                <a href="#">${contact.email}</a>
+            </div>
+        </div>
+`;
+}
+
 function showAddContact(parameter) {
-    let contactCard = document.getElementById("contact-card");
+  let contactCard = document.getElementById("contact-card");
   if (parameter == "show") {
     contactCard.classList.add("show-contact-container");
     contactCard.classList.remove("remove-contact-container");
@@ -10,24 +121,25 @@ function showAddContact(parameter) {
 }
 
 function showEditContact(parameter) {
-    let editCard = document.getElementById("edit-card");
-    if(parameter == 'show') {
-      editCard.classList.add("show-contact-container");
-      editCard.classList.remove("remove-contact-container");
-    } else {
-      editCard.classList.add("remove-contact-container");
-      editCard.classList.remove("show-contact-container");
-    }
+  let editCard = document.getElementById("edit-card");
+  if (parameter == "show") {
+    editCard.classList.add("show-contact-container");
+    editCard.classList.remove("remove-contact-container");
+  } else {
+    editCard.classList.add("remove-contact-container");
+    editCard.classList.remove("show-contact-container");
+  }
 }
 
 function doNotClose(event) {
   event.stopPropagation();
 }
 
-function showContact() {
+function showContact(i) {
   let menu = document.getElementById("contact-detail-data");
   menu.classList.toggle("remove-contact-detail");
-  let contact = document.getElementById("contact");
+
+  let contact = document.getElementById(`contact${i}`);
   if (contact.style.backgroundColor === "var(--customized_darkblue)") {
     contact.style.backgroundColor = "";
     contact.style.color = "";
