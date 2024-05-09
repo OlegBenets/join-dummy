199d2497cbe08c76c2bc2155c527ea94d9048787
Sascha Siegert <s.siegert85@googlemail.com>
2024-05-09 17:25:19 +0200

    create storage js

---
+++ js/storage.js
@@ -0,0 +1,21 @@
+let contacts = [
+    {
+        "id": "",
+        "name": "",
+        "email": "",
+        "password": "",
+        "phone": ""
+    }
+]
+let tasks =[
+    {
+        "asigntTo":[],
+        "category":true,
+        "date":"",
+        "id":"",
+        "prio":"",
+        "status":"",
+        "subTasks":"",
+        "title":""        
+    }
+]
