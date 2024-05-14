9bf1a84ab2b0da246d6a017c39d4b3ddc2ceb0bf
Patrick Batke <patrick.batke@web.de>
2024-05-14 19:03:48 +0200

    added add task confirmation

--- js/add_task.js
+++ js/add_task.js
@@ -3,16 +3,16 @@ let currentPrio = 'Medium';
 let currentStatus;
 
 function addTask() {
-    let title = document.getElementById('input-title');
-    let description = document.getElementById('input-description');
-    let asigntTo = document.getElementById('input-asignTo');
-    let date = document.getElementById('input-date');
+    let title = document.getElementById('input-title').value;
+    let description = document.getElementById('input-description').value;
+    let asigntTo = document.getElementById('input-asignTo').value;
+    let date = document.getElementById('input-date').value;
     let categoryTxt = document.getElementById('input-category').value;
     let category;
     let prio = currentPrio;
     let status = currentStatus;
     maxId++
-    
+
     if (categoryTxt == 'User Story') {
         category = true;
     }
@@ -20,24 +20,44 @@ function addTask() {
         category = false;
     }
 
-    let task = {
-        "asigntTo": [asigntTo.value],
-        "category":category,
-        "checkedSubtasks": 0,
-        "date":date.value,
-        "description": description.value,
-        "id": maxId,
-        "prio": prio,
-        "status":status,
-        "subTasks":['test', 'test2'],
-        "title": title.value
-    }
+    let task = creatTask([asigntTo], category, 0, date, description, maxId, prio, status, ['test1', 'test2'], title);
 
     tasks.push(task);
     loadCards();
     resetForm();
+    showAddTaskConfirmation();
+    
+    
 
-    console.log(tasks);
+    // let task = {
+    //     "asigntTo": [asigntTo.value],
+    //     "category":category,
+    //     "checkedSubtasks": 0,
+    //     "date":date.value,
+    //     "description": description.value,
+    //     "id": maxId,
+    //     "prio": prio,
+    //     "status":status,
+    //     "subTasks":['test', 'test2'],
+    //     "title": title.value
+    // }
+
+    // tasks.push(task);
+    // loadCards();
+    // resetForm();
+
+    // console.log(tasks);
+}
+
+function showAddTaskConfirmation() {
+    let confirmation = document.getElementById('add-task-confirmation-background');
+
+    confirmation.style.display = 'flex';
+
+    setTimeout(function() {
+        confirmation.style.display = 'none';
+        showMovableContainer('remove', 'addTask');
+    }, 1000);
 }
 
 
