53fcc4246e73509d2ce29c2650c2bc18bc11f49d
Oleg Benets <oleg.benets@web.de>
2024-05-19 16:01:10 +0200

    implemented assignContactToTask function

--- js/add_task.js
+++ js/add_task.js
@@ -1,6 +1,7 @@
 let currentPrio = "Medium";
 let currentStatus;
 let currentSubtasks = [];
+let assignedContacts = [];
 
 async function initPage() {
   await initInclude();
@@ -63,78 +64,74 @@ function resetForm() {
   let date = (document.getElementById("input-date").value = "");
   let categoryTxt = (document.getElementById("input-category").value = "");
   currentSubtasks = [];
-  document.getElementById('subtasks-popup-section').innerHTML = '';
+  document.getElementById("subtasks-popup-section").innerHTML = "";
 }
 
 function addSubtaskToPopup(parameter, index) {
-    let subtitle;
-    if (parameter === 'addTask') {
-        subtitle = document.getElementById('subtasks-input').value;
-    } 
-    else {
-        subtitle = document.getElementById('subtasks-input'+index).value;
-    }
+  let subtitle;
+  if (parameter === "addTask") {
+    subtitle = document.getElementById("subtasks-input").value;
+  } else {
+    subtitle = document.getElementById("subtasks-input" + index).value;
+  }
 
-    let subtask = creatSubTask(subtitle, checked = "unchecked");
+  let subtask = creatSubTask(subtitle, (checked = "unchecked"));
 
-    currentSubtasks.push(subtask);
-    renderSubtasksInPopup(parameter, index);
+  currentSubtasks.push(subtask);
+  renderSubtasksInPopup(parameter, index);
 }
 
 function renderSubtasksInPopup(parameter, index) {
-    let container;
-    if (parameter === 'addTask') {
-        container = document.getElementById('subtasks-popup-section');
-        container.innerHTML = '';
-    }
-    else {
-        container = document.getElementById('subtasks-popup-section'+index);
-    }
+  let container;
+  if (parameter === "addTask") {
+    container = document.getElementById("subtasks-popup-section");
+    container.innerHTML = "";
+  } else {
+    container = document.getElementById("subtasks-popup-section" + index);
+  }
 
+  for (let i = 0; i < currentSubtasks.length; i++) {
+    const subtask = currentSubtasks[i];
 
-    for (let i = 0; i < currentSubtasks.length; i++) {
-        const subtask = currentSubtasks[i];
-
-        container.innerHTML += generateHTMLsubtasksPopup(subtask);
-    }
+    container.innerHTML += generateHTMLsubtasksPopup(subtask);
+  }
 }
 
 function generateHTMLsubtasksPopup(subtask) {
-    return `
+  return `
     <div class='subtask-popup-edit-container'>
         <ul class='subtask-popup-ul-container'>
             <li>
-                ${subtask['subtitle']}  
+                ${subtask["subtitle"]}  
             </li>
             <div class='subtasks-edit-delete-container'>
-                <div class='subtasks-edit-container' onclick="editSubtask('${subtask['subtitle']}')">
+                <div class='subtasks-edit-container' onclick="editSubtask('${subtask["subtitle"]}')">
                     <img src='/assets/img/edit_normal.svg'>
                 </div>
                 <div class='subtasks-seperator'></div>
-                <div onclick="deleteSubtask('${subtask['subtitle']}')" class='subtasks-delete-container'>
+                <div onclick="deleteSubtask('${subtask["subtitle"]}')" class='subtasks-delete-container'>
                     <img src='/assets/img/delete.svg'>
                 </div>
             </div>
         </ul>
     </div>
-    `
+    `;
 }
 
 function editSubtask(subtaskTitle) {
-    // Finde den Container
-    const container = document.querySelector('.subtask-popup-edit-container');
+  // Finde den Container
+  const container = document.querySelector(".subtask-popup-edit-container");
 
-    // Generiere den HTML-Code für den Subtask
-    const subtaskHTML = generateSubtaskHTML(subtaskTitle);
+  // Generiere den HTML-Code für den Subtask
+  const subtaskHTML = generateSubtaskHTML(subtaskTitle);
 
-    // Füge den generierten HTML-Code dem Container hinzu
-    container.innerHTML = subtaskHTML;
-    
+  // Füge den generierten HTML-Code dem Container hinzu
+  container.innerHTML = subtaskHTML;
 }
 
 function generateSubtaskHTML(subtaskTitle) {
-    console.log(subtaskTitle);
-    return `
+  console.log(subtaskTitle);
+  return `
     <div class="subtask-popup-edit-container">
         <div class='display-flex'>
             <input type="text" value="${subtaskTitle}" class="subtask-edit-input">
@@ -150,44 +147,41 @@ function generateSubtaskHTML(subtaskTitle) {
     </div>`;
 }
 
-
 function deleteSubtask(subtask) {
-    let indexOfCurSubTask = currentSubtasks.findIndex(item => item === subtask);
-    currentSubtasks.splice(indexOfCurSubTask, 1);
-    renderSubtasksInPopup('addTask', '');
+  let indexOfCurSubTask = currentSubtasks.findIndex((item) => item === subtask);
+  currentSubtasks.splice(indexOfCurSubTask, 1);
+  renderSubtasksInPopup("addTask", "");
 }
 
 function clearSubtaskInput(parameter, index) {
-    if (parameter === 'addTask') {
-        document.getElementById('subtasks-input').value = '';
-    }
-    else {
-        document.getElementById('subtasks-input'+index).value = '';
-    }
+  if (parameter === "addTask") {
+    document.getElementById("subtasks-input").value = "";
+  } else {
+    document.getElementById("subtasks-input" + index).value = "";
+  }
 }
 
 function checkInput(parameter, index) {
-    let inputField;
-    let emptyInputImg;
-    let fullInputImgs;
-    if (parameter === 'addTask') {
-        inputField = document.getElementById('subtasks-input');
-        emptyInputImg = document.getElementById('subtasks-popup-empty-img');
-        fullInputImgs = document.getElementById('subtasks-popup-full-img');
-    }
-    else {
-        inputField = document.getElementById('subtasks-input'+index);
-        emptyInputImg = document.getElementById('subtasks-popup-empty-img'+index);
-        fullInputImgs = document.getElementById('subtasks-popup-full-img'+index);
-    }
+  let inputField;
+  let emptyInputImg;
+  let fullInputImgs;
+  if (parameter === "addTask") {
+    inputField = document.getElementById("subtasks-input");
+    emptyInputImg = document.getElementById("subtasks-popup-empty-img");
+    fullInputImgs = document.getElementById("subtasks-popup-full-img");
+  } else {
+    inputField = document.getElementById("subtasks-input" + index);
+    emptyInputImg = document.getElementById("subtasks-popup-empty-img" + index);
+    fullInputImgs = document.getElementById("subtasks-popup-full-img" + index);
+  }
 
-    if (inputField.value.trim() !== '') {
-        emptyInputImg.classList.add('display-none');
-        fullInputImgs.style.display = 'flex';
-    } else {
-        emptyInputImg.classList.remove('display-none');
-        fullInputImgs.style.display = 'none';
-    }
+  if (inputField.value.trim() !== "") {
+    emptyInputImg.classList.add("display-none");
+    fullInputImgs.style.display = "flex";
+  } else {
+    emptyInputImg.classList.remove("display-none");
+    fullInputImgs.style.display = "none";
+  }
 }
 
 function getPrio(prio) {
@@ -223,38 +217,51 @@ function testTask() {
   checkInputValidation(isTitleValid, isDateValid, titleInput, dateInput);
 }
 
-function checkInputValidation(isTitleValid, isDateValid, titleInput, dateInput) {
+function checkInputValidation(
+  isTitleValid,
+  isDateValid,
+  titleInput,
+  dateInput
+) {
   let titleError = document.getElementById("title-error");
   let dateError = document.getElementById("date-error");
-    // Validierung für den Titel
-    if (!isTitleValid) {
-      setErrorBorder(titleInput.parentElement);
-      titleError.querySelector(".error-text").style.display = "block";
-    } else {
-      removeErrorBorder(titleInput.parentElement);
-      titleError.querySelector(".error-text").style.display = "none";
-    }
-    // Validierung für das Datum
-    if (!isDateValid) {
-      setErrorBorder(dateInput.parentElement);
-      dateError.querySelector(".error-text").style.display = "block";
-    } else {
-      removeErrorBorder(dateInput.parentElement);
-      dateError.querySelector(".error-text").style.display = "none";
-    }
+  // Validierung für den Titel
+  if (!isTitleValid) {
+    setErrorBorder(titleInput.parentElement);
+    titleError.querySelector(".error-text").style.display = "block";
+  } else {
+    removeErrorBorder(titleInput.parentElement);
+    titleError.querySelector(".error-text").style.display = "none";
+  }
+  // Validierung für das Datum
+  if (!isDateValid) {
+    setErrorBorder(dateInput.parentElement);
+    dateError.querySelector(".error-text").style.display = "block";
+  } else {
+    removeErrorBorder(dateInput.parentElement);
+    dateError.querySelector(".error-text").style.display = "none";
+  }
 }
 
 function setEventlister() {
-  let inputLeft = document.getElementById("title-container").parentElement.querySelectorAll('.add-task-input-container');
-  let inputRight = document.getElementById("date-container").parentElement.querySelectorAll('.add-task-input-container');
+  let inputLeft = document
+    .getElementById("title-container")
+    .parentElement.querySelectorAll(".add-task-input-container");
+  let inputRight = document
+    .getElementById("date-container")
+    .parentElement.querySelectorAll(".add-task-input-container");
   let inputs = [inputLeft, inputRight];
   for (let j = 0; j < inputs.length; j++) {
     const nodeList = inputs[j];
     for (let i = 0; i < nodeList.length; i++) {
-        const element = nodeList[i].children[0];
-        element.addEventListener("focus", (event) => {setBorder(event)});
-        element.addEventListener("blur", (event) => {unsetBorder(event)});
-      }
+      const element = nodeList[i].children[0];
+      element.addEventListener("focus", (event) => {
+        setBorder(event);
+      });
+      element.addEventListener("blur", (event) => {
+        unsetBorder(event);
+      });
+    }
   }
 }
 
@@ -268,7 +275,6 @@ function unsetBorder(event) {
   target.parentElement.classList.remove("focused");
 }
 
-
 function searchContact() {
   //   let search = document.getElementById('input-assignTo').value;
   //   if (search.length >= 3) {
@@ -281,90 +287,101 @@ function searchContact() {
 }
 
 function showContactsToAssign() {
-  let contactAssignList = document.getElementById('contacts-list');
+  let contactAssignList = document.getElementById("contacts-list");
 
   // contactList.add.classList('.contacts-list');
-  contactAssignList.classList.toggle('contacts-list');
+  contactAssignList.classList.toggle("contacts-list");
   renderContactList();
 }
 
 function renderContactList() {
-  let contactList = document.getElementById('contacts-list');
-  if (contactList.classList.contains('contacts-list')) {
+  let contactList = document.getElementById("contacts-list");
+  if (contactList.classList.contains("contacts-list")) {
     contactList.innerHTML = "";
 
-  for (let i = 0; i < contacts.length; i++) {
-    const assignContacts = contacts[i];
-    let { initials, name } = extractInitialsAndName(assignContacts);
-    contactList.innerHTML += contactListTemplate(initials, name, assignContacts, i); 
+    for (let i = 0; i < contacts.length; i++) {
+      const ASSIGN_CONTACT = contacts[i];
+      let { initials, name } = extractInitialsAndName(ASSIGN_CONTACT);
+      contactList.innerHTML += contactListTemplate(
+        initials,
+        name,
+        ASSIGN_CONTACT,
+        i
+      );
+    }
+    console.log("contact list", contacts);
+  } else {
+    contactList.innerHTML = "";
   }
-  console.log("contact list", contacts);
-} else {
-  contactList.innerHTML = "";
-}
 }
 
-function contactListTemplate(initials, name, assignContacts, i) {
-  return /*html*/`
-    <div onclick="assignContactToTask(${i})" id="assigntContact${i}" class="assigned-contact-container">
-      <div class="assigned-contact-initials" style="background-color: #${assignContacts.color};">
+function contactListTemplate(initials, name, ASSIGN_CONTACT, i) {
+  return /*html*/ `
+    <div onclick="assignContactToTask(${i},'${name}')" id="assigntContact${i}" class="assigned-contact-container">
+    <div class="assigned-contact-child-container">
+      <div class="assigned-contact-initials" style="background-color: #${ASSIGN_CONTACT.color};">
         <h4>${initials}</h4>
       </div>
       <span class="assigned-contact-name">${name}</span>
+      </div>
+   <div class="checkbox login">
+        <div id="assignt-to${i}">
+            <img class="uncheck-contact" id="img_check_off${i}" src="../assets/img/checkbox_unselected.svg" alt="">
+            <img class="check-contact" id="img_check_on${i}" src="../assets/img/checked_white.svg" alt="">
+        </div> 
     </div>
 
-    <!-- <div class="checkbox login">
-                <input name="remember" type="checkbox" id="rememberMe">
-                <label for="rememberMe">
-
-                    <img class="checkOff" id="img_check_off" src="./assets/img/checkbox_unselected.svg" alt="">
-                    <img class="checkOn" id="img_check_on" src="./assets/img/checkbox_selected.svg" alt="">
-
-                    <div>Remember Me</div>
-                </label>
-            </div> -->
     `;
 }
 
-function assignContactToTask(i) {
+function assignContactToTask(i, name) {
   let assignContact = document.getElementById(`assigntContact${i}`);
+  let imgCheckOff = document.getElementById(`img_check_off${i}`);
+  let imgCheckOn = document.getElementById(`img_check_on${i}`);
 
   assignContact.style.backgroundColor = assignContact.style.backgroundColor === "var(--customized_darkblue)" ? "" : "var(--customized_darkblue)";
   assignContact.style.color = assignContact.style.color === "white" ? "" : "white";
+
+  if (imgCheckOff.style.display === "none") {
+    imgCheckOff.style.display = "block";
+    imgCheckOn.style.display = "none";
+    pushContactInArray(name);
+  } else {
+    imgCheckOff.style.display = "none";
+    imgCheckOn.style.display = "block";
+    removeContactInArray(name);
+  }
+
+  renderSelectedContacts();
 }
 
+function pushContactInArray(name) {
+  console.log('unchecked');
+  let index = assignedContacts.findIndex(n => n == name);
+  assignedContacts.splice(index, 1);
+  console.log(assignedContacts);
+}
 
-// .checkOff {
-//   display: block;
-//   width: 24px;
-//   height: 24px;
-// }
+function removeContactInArray(name) {
+  console.log('Checked');
+  assignedContacts.push(name);
+  console.log(assignedContacts);
+}
 
-// .checkOn {
-//   display: none;
-// }
+function renderSelectedContacts() {
+  let selectedContactsContainer = document.getElementById("selected-contacts");
+  selectedContactsContainer.innerHTML = "";
 
-// .checkOff:hover {
-//   border-radius: 6px;
-//   background-color: var(--customized_paleblue);
-// }
+  for (let contact of assignedContacts) {
+    let { initials, color } = extractInitialsAndColor(contact);
+    selectedContactsContainer.innerHTML += renderInitialsIcon(initials, color);
+  }
+}
 
-// .checkOn:hover {
-//   border-radius: 6px;
-//   background-color: var(--customized_paleblue);
-// }
-
-// .checkbox input {
-//   display: none;
-// }
-
-// .checkbox input:checked+label .checkOff {
-//   display: none;
-// }
-
-// .checkbox input:checked+label .checkOn {
-//   display: block;
-//   width: 24px;
-//   height: 24px;
-
-// }
+function renderInitialsIcon(initials, color) {
+  return /*html*/ `
+    <div class="assigned-contact-initials" style="background-color: #${color};">
+      <h4>${initials}</h4>
+    </div>
+  `;
+}
