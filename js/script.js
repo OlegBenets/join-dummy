ce1ae0b26b673831030040f0e1e71459b55498b5
Sascha Siegert <s.siegert85@googlemail.com>
2024-05-20 17:43:46 +0200

    reorganize the file structure and script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
-window.onload = loginCheck();
+window.onload = init();
+
+
+function init() {
+    loadScript();
+}
 
 function loginCheck() {
     let onLogin = window.location.pathname.includes("login.html");
@@ -15,4 +20,74 @@ function loginCheck() {
             window.location.href = '../login.html'
         }
     }
+}
+
+function loadScript() {
+
+    let head = document.querySelector('head');
+
+    console.log(window.location.pathname);
+    switch (window.location.pathname) {
+        case '/html/login.html':
+
+            head.innerHTML += `
+            <script src="../js/storage.js"></script>
+            <script src="../js/password_shower.js"></script>
+            <script src="../js/form_validation.js"></script>
+            `;
+            break;
+
+        case '/html/signup.html':
+            head.innerHTML += `
+            <script src="../js/password_shower.js"></script>
+            <script src="../js/storage.js"></script>
+            <script src="../js/form_validation.js"></script>
+            `;
+            break;
+
+        case '/html/summary.html':
+            head.innerHTML += `
+    
+                `;
+            break;
+        case '/html/help.html':
+            head.innerHTML += `
+
+            `;
+            break;
+        case '/html/contacts.html':
+            head.innerHTML += `
+
+            `;
+            break;
+        case '/html/add_task.html':
+            head.innerHTML += `
+
+            `;
+            break;
+        case '/html/board.html':
+            head.innerHTML += `
+
+            `;
+            break;
+        case '/html/index.html':
+            head.innerHTML += `
+
+            `;
+            break;
+        case '/html/legal_notice.html':
+            head.innerHTML += `
+
+            `;
+            break;
+        case '/html/privacy_policy.html':
+            head.innerHTML += `
+
+            `;
+            break;
+
+        default:
+            break;
+    }
+
 }
