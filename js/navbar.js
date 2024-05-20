8a2af2fe1fbac4f5e3aa747437b5e740bac71c77
Oleg Benets <oleg.benets@web.de>
2024-05-20 14:59:04 +0200

    comment out navbar.js

--- js/navbar.js
+++ js/navbar.js
@@ -1,34 +1,34 @@
 function selectMenu(menu) {
-    const SUMMARY_MENU = document.getElementById('summary-menu');
-    const ADDTASK_MENU = document.getElementById('addTask-menu');
-    const BOARD_MENU = document.getElementById('board-menu');
-    const CONTACTS_MENU = document.getElementById('contacts-menu');
+    // const SUMMARY_MENU = document.getElementById('summary-menu');
+    // const ADDTASK_MENU = document.getElementById('addTask-menu');
+    // const BOARD_MENU = document.getElementById('board-menu');
+    // const CONTACTS_MENU = document.getElementById('contacts-menu');
 
-    SUMMARY_MENU.classList.remove('selected-menu');
-    ADDTASK_MENU.classList.remove('selected-menu');
-    BOARD_MENU.classList.remove('selected-menu');
-    CONTACTS_MENU.classList.remove('selected-menu');
+    // SUMMARY_MENU.classList.remove('selected-menu');
+    // ADDTASK_MENU.classList.remove('selected-menu');
+    // BOARD_MENU.classList.remove('selected-menu');
+    // CONTACTS_MENU.classList.remove('selected-menu');
 
-    switch (menu) {
-        case 'summary':
-            window.location.href = '/summary/summary.html';
-            SUMMARY_MENU.classList.add('selected-menu');
-            break;
-        case 'addTask':
-            window.location.href = '/add_task/add_task.html';
-            ADDTASK_MENU.classList.add('selected-menu');
-            break;
-        case 'board':
-            window.location.href = '/board/board.html';
-            BOARD_MENU.classList.add('selected-menu');
-            break;
-        case 'contacts':
-            window.location.href = '/contacts/contacts.html';
-            CONTACTS_MENU.classList.add('selected-menu');
-            break;
+    // switch (menu) {
+    //     case 'summary':
+    //         window.location.href = '/summary/summary.html';
+    //         SUMMARY_MENU.classList.add('selected-menu');
+    //         break;
+    //     case 'addTask':
+    //         window.location.href = '/add_task/add_task.html';
+    //         ADDTASK_MENU.classList.add('selected-menu');
+    //         break;
+    //     case 'board':
+    //         window.location.href = '/board/board.html';
+    //         BOARD_MENU.classList.add('selected-menu');
+    //         break;
+    //     case 'contacts':
+    //         window.location.href = '/contacts/contacts.html';
+    //         CONTACTS_MENU.classList.add('selected-menu');
+    //         break;
     
-        default:
-            break;
-    }
+    //     default:
+    //         break;
+    // }
 }
 
