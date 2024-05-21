25922c228d62a8d294dd7412a207a27b1d721878
Oleg Benets <oleg.benets@web.de>
2024-05-21 19:47:07 +0200

    fixed logedUserData 1

--- js/summary.js
+++ js/summary.js
@@ -97,13 +97,13 @@ function setDayTime() {
     let greeting;
   
     if (hours >= 5 && hours < 12) {
-      greeting = "Godd morning";
+      greeting = "Godd morning,";
     } else if (hours >= 12 && hours < 18) {
-      greeting = "Good afternoon";
+      greeting = "Good afternoon,";
     } else if (hours >= 18 && hours < 22) {
-      greeting = "Good evening";
+      greeting = "Good evening,";
     } else {
-      greeting = "Good night";
+      greeting = "Good night,";
     }
     greetingElement.textContent = greeting;
 }
