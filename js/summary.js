68b104980b427249ca171670ed6ca3f8041b7f09
Oleg Benets <oleg.benets@web.de>
2024-05-21 16:39:43 +0200

    added ids to summary and variabels

---
+++ js/summary.js
@@ -0,0 +1,9 @@
+function documents() {
+    let todoAmount = document.getElementById('todo-amount').innerHTML;
+    let doneAmount = document.getElementById('done-amount').innerHTML;
+    let prioAmount = document.getElementById('prio-amount').innerHTML;
+    let deadline = document.getElementById('deadline').innerHTML;
+    let allTasksInBoard = document.getElementById('amount-tasks-in-board').innerHTML;
+    let progressAmount = document.getElementById('amount-tasks-progress').innerHTML;
+    let feedbackAmount = document.getElementById('amount-tasks-feedback').innerHTML;
+}
