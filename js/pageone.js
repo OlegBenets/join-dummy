e8bf661545661787d69d2a2dec80006b76124200
Sascha Siegert <s.siegert85@googlemail.com>
2024-05-26 20:32:48 +0200

    finish pageone .js .html .css

--- js/pageone.js
+++ js/pageone.js
@@ -13,14 +13,22 @@ function startPage() {
 
 
 /**
- * Applies a sliding animation to the logo.
- * 
- * This function selects the element with the ID 'content' and adds the class 'shrink_n_slide' to it,
- * which triggers a sliding animation defined in the CSS.
+ * Adjusts the logo display and behavior based on the window width.
+ * - Shrinks and slides the logo.
+ * - Changes the visibility of dark and light logos.
+ * - Adds a background class to the parent element of the logo if the window width is less than or equal to 1055 pixels.
  */
 function slideLogo() {
+    let width = window.innerWidth;
     let logo = document.getElementById('content');
+    let imgDark = document.getElementById('logoDark');
+    let imgLight = document.getElementById('logoLight');
     logo.classList.add('shrink_n_slide');
+    if (width <= 1055) {
+        imgDark.classList.add('show');
+        imgLight.classList.add('hide');
+        logo.parentElement.classList.add('background');
+    }
 }
 
 
